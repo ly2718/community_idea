f20b6398961b0ca4b92fe449658bd0f86e5acded
YukiNoHara <liyuan314@outlook.com>
2020-09-12 23:49:07 +0800

    init project

---
+++ src/main/java/com/liyuan/community/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.liyuan.community.controller;
+
+import com.liyuan.community.dto.AccessTokenDto;
+import com.liyuan.community.dto.GithubUser;
+import com.liyuan.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthController {
+    @Autowired
+    private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setCode(code);
+        accessTokenDto.setRedirect_uri(redirectUri);
+        accessTokenDto.setState(state);
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
+        String accessToken = githubProvider.getAccessToken(accessTokenDto);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user);
+        return "index";
+    }
+}
